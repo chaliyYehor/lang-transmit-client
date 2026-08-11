1cd44b72ae271f95b349e8ecbb2f7c951509ff04
Yehor <chaliyegor00@gmail.com>
2026-08-11 23:00:42 +0300

    merge

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,15 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
 import { qrcode } from 'vite-plugin-qrcode';
-=======
->>>>>>> 3aaae446b71b40f6d137d8d056658b38607dfd69
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-<<<<<<< HEAD
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
     qrcode()
-=======
-    babel({ presets: [reactCompilerPreset()] })
->>>>>>> 3aaae446b71b40f6d137d8d056658b38607dfd69
   ],
 })
