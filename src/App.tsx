d48e7cb244449cb87d3f62e4b82143635de13f18
Yehor <chaliyegor00@gmail.com>
2026-08-11 23:51:26 +0300

    add a snowfall background effect and a button whether to turn it on

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,15 @@ import type { AppDispatch, RootType } from './store/store'
 import { setConnected, setDisconnected } from './store/slices/isConnected'
 import { clearGlobalError, setGlobalError } from './store/slices/globalError'
 import { socket } from './socket'
-import { Moon, Sun } from 'lucide-react'
+import { Moon, Snowflake, Sun } from 'lucide-react'
+import Snowfall from 'react-snowfall'
 
 function App() {
 	const [dark, setDark] = useState(false)
+
+	const [isSnowing, setIsSnowing] = useState(
+		localStorage.getItem('isSnowing') === 'true',
+	)
 
 	useEffect(() => {
 		if (localStorage.getItem('theme')) {
@@ -81,15 +86,40 @@ function App() {
 			return !prev
 		})
 	}
+
+	function setIsSnowingFunc(value: boolean) {
+		setIsSnowing(value)
+		localStorage.setItem('isSnowing', value.toString())
+	}
 	return (
 		<>
-			<button
-				className='anim absolute z-10 top-[5%] sm:top-[50%] left-10 translate-y-[50%] w-10 h-10 flex justify-center items-center cursor-pointer'
-				onClick={() => switchTheme()}
-			>
-				{dark ? <Sun size={40} color='white' /> : <Moon size={40} color='black' />}
-			</button>
-
+			{isSnowing && (
+				<div className='snowfall-wrapper bg-transparent w-full h-screen absolute top-0 left-0 pointer-events-none'>
+					<Snowfall radius={[1, 3]} snowflakeCount={15} />
+				</div>
+			)}
+			<div className='absolute flex sm:flex-col gap-4 w-full justify-between p-5 sm:p-0 sm:justify-baseline sm:w-fit  z-10 sm:top-[50%] sm:left-10 '>
+				<button
+					className='anim w-10 h-10 flex justify-center items-center cursor-pointer'
+					onClick={() => switchTheme()}
+				>
+					{dark ? (
+						<Sun size={40} color='white' />
+					) : (
+						<Moon size={40} color='black' />
+					)}
+				</button>
+				<button
+					className='anim  w-10 h-10 flex justify-center items-center cursor-pointer'
+					onClick={() => setIsSnowingFunc(!isSnowing)}
+				>
+					{dark ? (
+						<Snowflake size={40} color='white' />
+					) : (
+						<Snowflake size={40} color='black' />
+					)}
+				</button>
+			</div>
 			<Routes>
 				<Route path='/' element={<JoinRoom />} />
 				<Route path='/room/:roomId' element={<Room />} />
