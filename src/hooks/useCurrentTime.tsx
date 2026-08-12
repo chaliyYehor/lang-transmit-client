a4f40878d4098f7e50304860a8822be9ccf2e12e
Yehor <chaliyegor00@gmail.com>
2026-08-12 20:08:35 +0300

    add a date

--- src/hooks/useCurrentTime.tsx
+++ src/hooks/useCurrentTime.tsx
@@ -9,16 +9,13 @@ export function useCurrentTime() {
       setTime(dayjs())
     }
 
-    // Сразу выставляем актуальное время
     updateTime()
 
-    // Сколько миллисекунд осталось до следующей минуты
     const delay = 60_000 - (Date.now() % 60_000)
 
     const timeout = setTimeout(() => {
       updateTime()
 
-      // После попадания в начало минуты обновляемся каждые 60 секунд
       const interval = setInterval(updateTime, 60_000)
 
       return () => clearInterval(interval)
@@ -30,5 +27,6 @@ export function useCurrentTime() {
   return {
     hours: time.format('HH'),
     minutes: time.format('mm'),
+    date: time.format('DD.MM.YYYY'),
   }
 }
