dbdc0bd890bf359108b12c48b1666264a5a74c7a
Yehor <chaliyegor00@gmail.com>
2026-08-12 20:17:01 +0300

    change reconnection times

--- src/socket.ts
+++ src/socket.ts
@@ -3,6 +3,5 @@ import { io } from 'socket.io-client'
 export const socket = io(import.meta.env.VITE_SERVER_URL, {
 	autoConnect: true,
 	reconnection: true,
-	reconnectionAttempts: 5,
 	reconnectionDelay: 500,
 })
